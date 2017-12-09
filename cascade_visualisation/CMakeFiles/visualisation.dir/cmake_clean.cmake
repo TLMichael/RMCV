92e6bb2da2f4514276b157fdf779ea473093cbb8
TLMichael <756535645@qq.com>
2017-12-09 21:50:17 +0800

    add test

---
+++ cascade_visualisation/CMakeFiles/visualisation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualisation.dir/opencv_visualisation.cpp.o"
+  "visualisation.pdb"
+  "visualisation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualisation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
