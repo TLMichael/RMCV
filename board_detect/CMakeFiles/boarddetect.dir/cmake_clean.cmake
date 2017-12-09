4c17567ceb523afe2b3abc38f6cc396ac6867e6b
TLMichael <756535645@qq.com>
2017-12-09 15:40:28 +0800

    Train two model.xml with 458 true samples. There are something I can improve.

---
+++ board_detect/CMakeFiles/boarddetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boarddetect.dir/boarddetect.cpp.o"
+  "boarddetect.pdb"
+  "boarddetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boarddetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
