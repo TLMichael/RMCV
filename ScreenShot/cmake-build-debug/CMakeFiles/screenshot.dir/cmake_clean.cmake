41b68e473328303830d3469b40bc2a906a4ae207
TLMichael <756535645@qq.com>
2017-12-23 14:42:32 +0800

    Add a project named "ScreenShot", which is designed to screenshot frame from video.

---
+++ ScreenShot/cmake-build-debug/CMakeFiles/screenshot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/screenshot.dir/excited.cpp.o"
+  "screenshot.pdb"
+  "screenshot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/screenshot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
